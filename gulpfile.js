3af1eedfb29d82304e6a55b04f8c360e0e5dbbcb
Marcel <uehdev@gmail.com>
2023-09-04 11:40:13 -0300

    Criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,30 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
   return gulp
     .src("./src/styles/*.scss")
-    .pipe(sass({ outputStyle: "compressed" }))
-    .pipe(gulp.dest("./dist/css"));
+      .pipe(sass({ outputStyle: "compressed" }))
+      .pipe(gulp.dest("./dist/css"));
 }
 
+function images() {
+  return gulp
+    .src("./src/images/**/*")
+      .pipe(imagemin())
+      .pipe(gulp.dest("./dist/images"));
+}
+
+/*
 function testeGulp(cb) {
   console.log("Olá mundo");
   cb();
 }
+*/
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
 };
